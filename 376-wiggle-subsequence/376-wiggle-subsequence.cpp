ede38f2af640bb994a51e036b112d9aa55cbaf0d
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-03 20:59:37 +0530

    Time: 3 ms (69.26%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
+        //Remove duplicate elements
         for(int i = 0; i < nums.size()-1; i++){
             if(nums[i] == nums[i+1]){
                 nums.erase(nums.begin()+i);
@@ -8,10 +9,12 @@ public:
             }
         }
         
+        //if size is less than or equal to 2, return it's size
         if(nums.size() <=  2){
             return nums.size();
         }
         
+        //Count the number of elements that have either smaller values before and after them or the elements that have greater values before and after them
         int count = 0;
         for( int i = 1; i < nums.size()-1; i++){
             if(nums[i] > nums[i-1] && nums[i] > nums[i+1] || nums[i] < nums[i-1] && nums[i] < nums[i+1]){
@@ -19,6 +22,7 @@ public:
             }
         }
         
+        //Add 2 to count for adding first and last element to the result
         return count+2;
     }
 };
