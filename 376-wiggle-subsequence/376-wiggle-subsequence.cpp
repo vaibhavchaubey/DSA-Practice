cde602dcc115bb7f6184d6912992ea2b37fbcb4a
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-03 22:10:09 +0530

    Time: 4 ms (56.21%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,28 +1,55 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        //Remove duplicate elements
-        for(int i = 0; i < nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-                nums.erase(nums.begin()+i);
-                i--;
-            }
-        }
-        
-        //if size is less than or equal to 2, return it's size
-        if(nums.size() <=  2){
+        if(nums.size() < 2){
             return nums.size();
         }
+        int prevDiff = nums[1] - nums[0];
+        int count = prevDiff != 0 ? 2 : 1;
         
-        //Count the number of elements that have either smaller values before and after them or the elements that have greater values before and after them
-        int count = 0;
-        for( int i = 1; i < nums.size()-1; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1] || nums[i] < nums[i-1] && nums[i] < nums[i+1]){
+        //if prevDiff is negative than diff will be positive and vice versa
+        for( int i = 2; i < nums.size(); i++){
+            int diff = nums[i] - nums[i-1];
+            if(diff > 0 && prevDiff <= 0 || diff < 0 && prevDiff >= 0){
                 count++;
+                prevDiff = diff;
             }
         }
         
-        //Add 2 to count for adding first and last element to the result
-        return count+2;
+        return count;
     }
 };
+
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         //Remove duplicate elements
+//         for(int i = 0; i < nums.size()-1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 nums.erase(nums.begin()+i);
+//                 i--;
+//             }
+//         }
+        
+//         //if size is less than or equal to 2, return it's size
+//         if(nums.size() <=  2){
+//             return nums.size();
+//         }
+        
+//         //Count the number of elements that have either smaller values before and after them or the elements that have greater values before and after them
+//         int count = 0;
+//         for( int i = 1; i < nums.size()-1; i++){
+//             if(nums[i] > nums[i-1] && nums[i] > nums[i+1] || nums[i] < nums[i-1] && nums[i] < nums[i+1]){
+//                 count++;
+//             }
+//         }
+        
+//         //Add 2 to count for adding first and last element to the result
+//         return count+2;
+//     }
+// };
