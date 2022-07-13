fe5c83b2a12e6737beac5acb34a01d8ad4aafe82
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-13 18:41:59 +0530

    Time: 7 ms (54.38%), Space: 12.5 MB (61.71%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        if(root == NULL){
+            return ans;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node){
+                v.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            else if(!q.empty()){
+                q.push(NULL);
+                ans.push_back(v);
+                v.clear();
+            }
+        }
+        ans.push_back(v);  //This is done because in last step else if do not execute
+        return ans;
+    }
+};
+
+/*       OR         */
+
+// class Solution {
+//      public:
+//         vector<vector<int>> levelOrder(TreeNode* root) {
+//             if(root==NULL)
+//                return {};
+//            vector<vector<int>> ans; 
+//             queue<TreeNode*> q;
+//             q.push(root);
+//             TreeNode *temp;
+//             int len;
+//             while(!q.empty()){
+//                len=q.size();
+// 		       vector<int> v;
+//                for(int i=0;i<len;i++){
+//                     temp=q.front();
+//                     q.pop();
+//                     v.push_back(temp->val);
+
+//                    if(temp->left) q.push(temp->left);
+//                    if(temp->right) q.push(temp->right);
+//                 }
+// 		        ans.push_back(v);
+//             }
+//           return ans;
+//         }
+//      };
