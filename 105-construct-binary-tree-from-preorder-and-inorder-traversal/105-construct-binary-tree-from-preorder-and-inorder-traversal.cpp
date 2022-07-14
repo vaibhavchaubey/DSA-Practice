3802b7ba60199763742feffae5ae3bd68a5dc8cf
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-14 21:47:42 +0530

    Time: 58 ms (26.48%), Space: 25.9 MB (79.05%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,7 +12,15 @@
 class Solution {
 public:
     int idx = 0;
-    TreeNode* constructBT(vector<int>& preorder, int start, int end, map<int, int>& mp){ 
+    int search(vector<int>& inorder, int start, int end, int curr){
+        for(int i = start; i <= end; i++){
+            if(inorder[i] == curr){
+                return i;
+            }
+        }
+        return -1;
+    }
+    TreeNode* constructBT(vector<int>& preorder, vector<int>& inorder, int start, int end){
         if(start > end){
             return NULL;
         }
@@ -22,20 +30,14 @@ public:
         if(start == end){
             return node;
         }
-        int pos = mp[curr];
-        node->left = constructBT(preorder, start, pos-1, mp);
-        node->right = constructBT(preorder, pos+1, end, mp);
+        int pos = search(inorder, start, end, curr);
+        node->left = constructBT(preorder, inorder, start, pos-1);
+        node->right = constructBT(preorder, inorder, pos+1, end);
         
         return node;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int start = 0, end = inorder.size()-1;
-        map<int, int> mp;
-        for(int i = start; i <= end; i++){
-            mp[inorder[i]] = i;
-        }
-        
-       TreeNode* root = constructBT(preorder, start, end, mp);
+       TreeNode* root = constructBT(preorder, inorder, 0, preorder.size()-1);
         
         return root;
     }
