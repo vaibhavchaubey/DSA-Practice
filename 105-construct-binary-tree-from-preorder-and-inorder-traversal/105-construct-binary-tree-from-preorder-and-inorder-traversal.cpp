4bfe1ace4c789087bd63aa30fde5226484fcb824
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-14 22:03:41 +0530

    Time: 7 ms (99.84%), Space: 26.5 MB (17.58%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,15 +11,8 @@
  */
 class Solution {
 public:
-    int search(vector<int>& inorder, int start, int end, int curr){
-        for(int i = start; i <= end; i++){
-            if(inorder[i] == curr){
-                return i;
-            }
-        }
-        return -1;
-    }
-    TreeNode* constructBT(int& idx , vector<int>& preorder, vector<int>& inorder, int start, int end){
+    int idx = 0;
+    TreeNode* constructBT(vector<int>& preorder, int start, int end, unordered_map<int, int>& mp){ 
         if(start > end){
             return NULL;
         }
@@ -29,16 +22,63 @@ public:
         if(start == end){
             return node;
         }
-        int pos = search(inorder, start, end, curr);
-        node->left = constructBT(idx, preorder, inorder, start, pos-1);
-        node->right = constructBT(idx, preorder, inorder, pos+1, end);
+        int pos = mp[curr];
+        node->left = constructBT(preorder, start, pos-1, mp);
+        node->right = constructBT(preorder, pos+1, end, mp);
         
         return node;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int idx = 0;
-       TreeNode* root = constructBT(idx, preorder, inorder, 0, preorder.size()-1);
+        int start = 0, end = inorder.size()-1;
+        unordered_map<int, int> mp;
+        for(int i = start; i <= end; i++){
+            mp[inorder[i]] = i;
+        }
+        
+       TreeNode* root = constructBT(preorder, start, end, mp);
         
         return root;
     }
 };
+
+//TC - O(N)
+//SC - O(N)
+
+
+
+
+//OR       TC - O(N^2)
+
+// class Solution {
+// public:
+//     int idx = 0;
+//     int search(vector<int>& inorder, int start, int end, int curr){
+//         for(int i = start; i <= end; i++){
+//             if(inorder[i] == curr){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     TreeNode* constructBT(vector<int>& preorder, vector<int>& inorder, int start, int end){
+//         if(start > end){
+//             return NULL;
+//         }
+//         int curr = preorder[idx];
+//         idx++;
+//         TreeNode* node = new TreeNode(curr);
+//         if(start == end){
+//             return node;
+//         }
+//         int pos = search(inorder, start, end, curr);
+//         node->left = constructBT(preorder, inorder, start, pos-1);
+//         node->right = constructBT(preorder, inorder, pos+1, end);
+        
+//         return node;
+//     }
+//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+//        TreeNode* root = constructBT(preorder, inorder, 0, preorder.size()-1);
+        
+//         return root;
+//     }
+// };
