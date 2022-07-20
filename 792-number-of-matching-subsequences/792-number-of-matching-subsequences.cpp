468c3ecfa7a075889dd8840ee6b006f9ed035a46
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-20 10:03:36 +0530

    Time: 215 ms (86.96%), Space: 38 MB (87.39%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
         int count = 0;
-        map<string,int> mp;
+        //we will be form map to count occurences of particular word to avoid re calculation and this will reduce time
+        unordered_map<string,int> mp;
         for(int i= 0; i < words.size(); i++){
             mp[words[i]]++;
         }
@@ -17,10 +18,39 @@ public:
                 j++;
             }
             if(i == n){
-                count += x.second;
+                count += x.second;  //if we reach the length of string str then this word in in our string s. so we increase our count by number of times it occured in words instead of increamenting by 1
             }
         }
         
         return count++;
     }
 };
+
+
+
+//Brute Force 
+//(It will give TLE as in case where words are repeated we have to loop for the same word again and again)
+
+// class Solution {
+// public:
+//     int numMatchingSubseq(string s, vector<string>& words) {
+//         int count = 0;
+//         int i = 0, j = 0;
+//         for(int k = 0; k < words.size(); k++){
+//             int n = words[k].size();
+//             string str = words[k];
+//             i = 0, j = 0;
+//             while(i < n && j < s.size()){
+//                 if(str[i] == s[j]){
+//                     i++;
+//                 }
+//                 j++;
+//             }
+//             if(i == n){
+//                 count++;
+//             }
+//         }
+        
+//         return count++;
+//     }
+// };
