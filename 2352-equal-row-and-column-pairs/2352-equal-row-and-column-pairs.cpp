49e13c167f99b297e8d95db84b6ca7af2a5759fd
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-24 09:59:11 +0530

    Time: 636 ms (66.67%), Space: 23.4 MB (100.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    int equalPairs(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> mat(n,vector<int>(n,0));
+    int equalPairs(vector<vector<int>>& v) {
+        int n=v.size(),ans=0;
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                mat[i][j] = grid[j][i];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                bool flag=true;
+                for(int k=0;k<n;k++){
+                    if(v[i][k]!=v[k][j]){
+                        flag=false;break;
+                    }
+                }
+                ans+=flag;
             }
         }
         
-        int count = 0, cnt = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                count = 0;
-                for(int k = 0; k < n; k++){
-                    if(grid[i][k] == mat[j][k]){
-                        count++;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                if(count == n){
-                    cnt++;
-                }
-            }
-        }
-        
-        return cnt;
-
-        
+        return ans;
     }
 };
