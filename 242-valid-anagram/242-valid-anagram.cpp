bfe6148c9d6afabdfb23856c5e5bbb2344930d65
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-28 21:07:16 +0530

    Time: 23 ms (22.29%), Space: 7.2 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,26 +4,49 @@ public:
         if(s.size() != t.size()){
             return false;
         }
-        unordered_map<char,int> mp1;
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
         for(int i = 0; i < s.size(); i++){
-            mp1[s[i]]++;
-        }
-        
-        unordered_map<char,int> mp2;
-        for(int i = 0; i < t.size(); i++){
-            mp2[t[i]]++;
-        }
-        
-        if(mp1.size() == mp2.size()){
-            if(mp1 == mp2){
-                return true;
-            }
-            else{
+            if(s[i] != t[i]){
                 return false;
             }
         }
-        else{
-            return false;
-        }
+        return true;
     }
 };
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+        // if(s.size() != t.size()){
+        //     return false;
+        // }
+//         unordered_map<char,int> mp1;
+//         for(int i = 0; i < s.size(); i++){
+//             mp1[s[i]]++;
+//         }
+        
+//         unordered_map<char,int> mp2;
+//         for(int i = 0; i < t.size(); i++){
+//             mp2[t[i]]++;
+//         }
+        
+//         if(mp1.size() == mp2.size()){
+//             if(mp1 == mp2){
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// };
