151e3787c5d2394c61256b754157c47a92469330
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-28 21:01:21 +0530

    Time: 6 ms (90.64%), Space: 7.3 MB (77.97%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char,int> mp1;
+        for(int i = 0; i < s.size(); i++){
+            mp1[s[i]]++;
+        }
+        
+        unordered_map<char,int> mp2;
+        for(int i = 0; i < t.size(); i++){
+            mp2[t[i]]++;
+        }
+        
+        if(mp1.size() == mp2.size()){
+            if(mp1 == mp2){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+};
