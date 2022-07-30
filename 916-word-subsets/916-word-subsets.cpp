62e9344997ea682d7c25950c3ae31851c10d723c
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-07-30 22:12:02 +0530

    Time: 387 ms (35.28%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -7,7 +7,9 @@ public:
             vector<int> cur(26,0);
             for(auto c : word){
                 cur[c-'a']++;
-                maxCount[c-'a'] = max(maxCount[c-'a'],cur[c-'a']);
+            }
+            for(int i = 0; i < 26; i++){
+                maxCount[i] = max(maxCount[i],cur[i]);
             }
         }
         
@@ -31,45 +33,3 @@ public:
         return ans;
     }
 };
-
-
-
-
-
-//OR
-
-// class Solution {
-// public:
-//     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-//         vector<string> ans;
-//         vector<int> maxCount(26,0);
-//         for(auto word : words2){
-//             vector<int> cur(26,0);
-//             for(auto c : word){
-//                 cur[c-'a']++;
-//             }
-//             for(int i = 0; i < 26; i++){
-//                 maxCount[i] = max(maxCount[i],cur[i]);
-//             }
-//         }
-        
-//         for(auto word : words1){
-//             vector<int> cur(26,0);
-//             for(auto c : word){
-//                 cur[c-'a']++;
-//             }
-//             bool flag = true;
-//             for(int i = 0; i < 26; i++){
-//                 if(cur[i] < maxCount[i]){
-//                     flag = false;
-//                     break;
-//                 }
-//             }
-//             if(flag){
-//                 ans.push_back(word);
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
