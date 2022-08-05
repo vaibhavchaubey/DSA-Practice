2b33dafe9fbdd4ec6967d99696639908c868cb57
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-05 13:56:04 +0530

    Time: 12 ms (95.09%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        unordered_set<int> row;
-        unordered_set<int> col;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        int R = matrix.size(), C = matrix[0].size();
+        vector<int> row(R,1);
+        vector<int> col(C,1);
+        
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
                 if(matrix[i][j] == 0){
-                    row.insert(i);
-                    col.insert(j);
+                    row[i] = 0;
+                    col[j] = 0;
                 }
             }
         }
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(row.find(i) != row.end() || col.find(j) != col.end()){
+        
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(row[i] == 0 || col[j] == 0){ 
                     matrix[i][j] = 0;
                 }
             }
         }
+        
     }
 };
+
+//Time Complexity: O(M*N)  where M and N are the number of rows and columns respectively
+//Space Complexity: O(M+N)
