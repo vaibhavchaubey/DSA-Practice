d448ee40baf1c76684999d1bc57be9c7b5430126
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-05 13:32:41 +0530

    Time: 42 ms (5.24%), Space: 13.5 MB (10.32%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    void setRowZero(vector<int> row, int n, vector<vector<int>>& matrix){
-        for(auto it : row){
-            for(int i = 0; i < n; i++){
-                matrix[it][i] = 0;
-            }
-        }
-    }
-    void setColZero(vector<int> col, int m, vector<vector<int>>& matrix){
-        for(auto it : col){
-            for(int i = 0; i < m; i++){
-                matrix[i][it] = 0;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<int> row;
-        vector<int> col;
+        set<int> row;
+        set<int> col;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j] == 0){
-                    row.push_back(i);
-                    col.push_back(j);
+                    row.insert(i);
+                    col.insert(j);
                 }
             }
         }
-        setRowZero(row,n,matrix);
-        setColZero(col,m,matrix);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row.find(i) != row.end() || col.find(j) != col.end()){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
