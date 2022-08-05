bb7c3ee5d8f545103434ea6dba0e716e9afd14bb
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-05 14:45:09 +0530

    Time: 41 ms (5.24%), Space: 13.1 MB (81.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -30,3 +30,36 @@ public:
 
 //Time Complexity: O(M*N)  where M and N are the number of rows and columns respectively
 //Space Complexity: O(1)
+
+
+//OR
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int R = matrix.size(), C = matrix[0].size();
+//         vector<int> row(R,1);
+//         vector<int> col(C,1);
+        
+//         for(int i = 0; i < R; i++){
+//             for(int j = 0; j < C; j++){
+//                 if(matrix[i][j] == 0){
+//                     row[i] = 0;
+//                     col[j] = 0;
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0; i < R; i++){
+//             for(int j = 0; j < C; j++){
+//                 if(row[i] == 0 || col[j] == 0){ 
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+//     }
+// };
+
+//Time Complexity: O(M*N)  where M and N are the number of rows and columns respectively
+//Space Complexity: O(M+N)
