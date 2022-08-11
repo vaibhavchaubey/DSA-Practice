d26c21a8fbd2adc3784caa556482a2ac3d9ff1c4
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-11 19:06:02 +0530

    Time: 25 ms (26.75%), Space: 21.7 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,25 +11,16 @@
  */
 class Solution {
 public:
-    vector<int> v;
-    void inorder(TreeNode* root){
-        if(root == NULL){
-            return;
-        }
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
+    bool isValid(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root){
+            return true;
+        } 
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val){
+            return false;
+        }    
+        return isValid(root->left, minNode, root) && isValid(root->right, root, maxNode);
     }
-    
     bool isValidBST(TreeNode* root) {
-        inorder(root);
-        
-        for(int i = 0; i < v.size()-1; i++){
-            if(v[i] >= v[i+1]){
-                return false;
-            }
-        }
-        
-        return true;
+        return isValid(root, NULL, NULL);
     }
 };
