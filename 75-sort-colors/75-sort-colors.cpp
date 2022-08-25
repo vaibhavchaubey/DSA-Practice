6704f7612265a37fa00d795e8531cdd466a2cd44
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-25 23:50:11 +0530

    Time: 4 ms (45.32%), Space: 8.3 MB (70.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -8,8 +8,8 @@ public:
         while (mid <= high)
         {
             if (nums[mid] == 0){
-            swap(nums[low],nums[mid]);
-            low++; mid++;
+                swap(nums[low],nums[mid]);
+                low++; mid++;
             }
             else if (nums[mid] == 1){
                 mid++;
