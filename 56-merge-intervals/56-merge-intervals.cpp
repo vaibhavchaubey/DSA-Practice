95463a174eb3b8d9b58def4539aaab3b084baaba
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-26 18:20:48 +0530

    Time: 67 ms (43.33%), Space: 20.1 MB (25.48%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -22,3 +22,37 @@ public:
 
 //Time Complexity: O(NlogN)+O(N)
 //Space Complexity: O(N)
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         int n = intervals.size();
+//         vector<vector<int>> ans;
+//         sort(intervals.begin(), intervals.end());
+
+//         for(int i = 0; i < n; i++){
+//             int start = intervals[i][0], end = intervals[i][1];
+//             if(!ans.empty()){
+//                 if(start <= ans.back()[1]){
+//                     continue;
+//                 }
+//             }
+//             for(int j = i+1; j < n; j++){
+//                 if(intervals[j][0] <= end){
+//                     end = max(end, intervals[j][1]);
+//                 }
+//             }
+//             end = max(end, intervals[i][1]);
+//             ans.push_back({start,end});
+//         }
+        
+//         return ans;
+//     }
+// };
+
+//Time Complexity: O(NlogN)+O(N*N)
+//Space Complexity: O(N)
