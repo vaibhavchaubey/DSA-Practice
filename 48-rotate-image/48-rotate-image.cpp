598c8eee6b8a5fffb32cdca6660eda31c81eee10
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-26 16:15:29 +0530

    Time: 6 ms (23.23%), Space: 7.1 MB (33.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int n = matrix.size();
         //Transpose Matrix
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
+            for(int j = i+1; j < n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
