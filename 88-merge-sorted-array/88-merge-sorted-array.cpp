573b4e68d00defad9c592d7c1b4eac0e73a78ab2
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-27 13:09:57 +0530

    Time: 7 ms (29.44%), Space: 9 MB (90.53%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,9 +1,54 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = 0; i < nums2.size(); i++){
-            nums1[m + i] = nums2[i];
+        if(m == 0){
+            nums1 = nums2;
         }
-        sort(nums1.begin(), nums1.end());
+        else if(m != 0 && n != 0){
+            int k;
+            for(int i = 0; i < m; i++){
+                // take first element from arr1 
+                // compare it with first element of second array
+                // if condition match, then swap
+                if(nums1[i] > nums2[0]){
+                    swap(nums1[i], nums2[0]);              
+                }
+            
+                // then sort the second array
+                // put the element in its correct position
+                // so that next cycle can swap elements correctly
+                int first = nums2[0];
+                // insertion sort is used here
+                for(k = 1; k < n && nums2[k] < first; k++){
+                    nums2[k - 1] = nums2[k];
+                }
+                nums2[k - 1] = first;
+            }
+        
+            for(int i = 0; i < nums2.size(); i++){
+                nums1[m + i] = nums2[i];
+            }
+        } 
     }
 };
+
+//Time complexity: O(n*m)
+//Space Complexity: O(1) 
+
+
+
+//OR
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         for(int i = 0; i < nums2.size(); i++){
+//             nums1[m + i] = nums2[i];
+//         }
+//         sort(nums1.begin(), nums1.end());
+//     }
+// };
+
+//Time complexity: O(n*log(n))+O(n)
+//Space Complexity: O(1) 
+
+
