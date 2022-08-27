b99e004e40a5dce505222910a2e92b5f25432a3e
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-08-27 18:01:48 +0530

    Time: 349 ms (12.83%), Space: 61.7 MB (24.85%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,12 +2,41 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] == nums[i + 1]){
+        int freq[n + 1];
+        for(int i = 0; i < n + 1; i++){
+            freq[i] = 0;
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(freq[nums[i]] == 0){
+                freq[nums[i]]++;
+            }
+            else{
                 return nums[i];
             }
         }
         return -1;
     }
 };
+
+//Time Complexity:O(N)
+//Space Complexity:O(N)
+
+
+//OR
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+//         for(int i = 0; i < n - 1; i++){
+//             if(nums[i] == nums[i + 1]){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//Time Complexity:O(NlogN + N)
+//Space Complexity:O(1)
