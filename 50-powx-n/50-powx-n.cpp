54d6be64db0c195490cbf31a5a6be218fb828b42
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-03 00:10:31 +0530

    Time: 5 ms (21.08%), Space: 5.9 MB (81.50%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -28,6 +28,32 @@ public:
 
 
 
+//OR   //It will give TLE
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         double ans = 1.0;
+//         long long nn = n;
+//         if(n < 0){
+//             nn = -1 * nn;
+//         }
+//         for(int i = 0; i < nn; i++){
+//             ans = ans * x;
+//         }
+//         if(n >= 0){
+//             return ans;
+//         }
+//         else{
+//             return (1/ans);
+//         }
+//     }
+// };
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+
+
 
 
 
