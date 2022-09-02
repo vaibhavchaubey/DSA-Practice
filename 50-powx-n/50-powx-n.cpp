1d1e12bca21b0e9a50c7f898f09c0f1199403886
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-02 21:33:42 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.19%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+        if(n < 0){
+            nn = -1 * nn;
+        }
+        while(nn){
+            if(nn % 2 == 1){
+                ans = ans * x;
+                nn = nn - 1;
+            }
+            else{
+                x = x * x;
+                nn = nn/2;
+            }
+        }
+        if(n < 0){
+            return (double)(1.0) / (double)(ans);
+        }
+        return ans;
+    }
+};
+
+//Time Complexity: O(logN) 
+//Space Complexity: O(1)
+
+
+
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {  
+//         if(n == 0){
+//             return 1;
+//         }
+//         double y = myPow(x,n/2);
+//         if(n%2 == 0){
+//             return y*y;
+//         }
+//         else{
+//             return n < 0 ? 1/x*y*y : x*y*y;
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
