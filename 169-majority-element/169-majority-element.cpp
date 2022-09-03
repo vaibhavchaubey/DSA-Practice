1b319a03589168a049beced83fe247b42d1f5b23
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-03 23:21:27 +0530

    Time: 36 ms (35.26%), Space: 19.5 MB (92.30%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,11 +5,9 @@ public:
         unordered_map<int,int> mp;
         for(int i = 0; i < n; i++){
             mp[nums[i]]++;
-        }
-        for(auto it : mp){
-            if(it.second > n/2){
-                return it.first;
-            } 
+            if(mp[nums[i]] > n/2){
+                return nums[i];
+            }
         }
         return -1;
     }
