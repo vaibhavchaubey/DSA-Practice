cc1c5b4654606f617c3f72646f7afc5d9122b3b7
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-03 22:44:36 +0530

    Time: 56 ms (5.11%), Space: 19.7 MB (13.91%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,10 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        return nums[n/2];         //It always return the majority element
+        unordered_map<int,int> mp;
+        for(int i = 0; i < n; i++){
+            mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second > n/2){
+                return it.first;
+            } 
+        }
+        return -1;
     }
 };
 
-//Time complexity : O(NlogN)
-//Space complexity : O(1)
+//Time Complexity: O(N)-> Frequency array or O(N log N) -> HashMap 
+//Space Complexity: O(N)
+
+
