e00e6435a7420ecb41680874851c5ec365b788ee
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-05 20:56:04 +0530

    Time: 40 ms (24.41%), Space: 19.5 MB (60.06%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -24,3 +24,66 @@ public:
 //Space Complexity: O(1)
 
 
+
+//OR
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int> mp;
+//         for(int i = 0; i < n; i++){
+//             mp[nums[i]]++;
+//             if(mp[nums[i]] > n/2){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// Time Complexity: O(N)-> Frequency array or O(NlogN) -> HashMap 
+// Space Complexity: O(N)
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         return nums[n/2];         //It always return the majority element
+//     }
+// };
+
+// Time complexity : O(NlogN)
+// Space complexity : O(1)
+
+
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++){
+//             int cnt = 1;
+//             for(int j = i+1; j < n; j++){
+//                 if(nums[i] == nums[j]){
+//                     cnt++;
+//                 }
+//             }
+//             if(cnt > n/2){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// Time Complexity: O(N*N) 
+// Space Complexity: O(1) 
+
+
