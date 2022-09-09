ee172f4a86ad6417e9fa3c1825e030004b21b00e
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-10 00:06:34 +0530

    Time: 415 ms (83.30%), Space: 45.4 MB (82.94%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int merge(vector<int>& nums, int l, int mid, int r){
+        int pairs = 0;
+        int j = mid + 1;
+        for(int i = l; i <= mid; i++){
+            while(j <= r && nums[i] > 2LL * nums[j]){
+                j++;
+            }
+            pairs += (j - (mid + 1));
+        }
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int a[n1];
+        int b[n2];
+        for(int i = 0; i < n1; i++){
+            a[i] = nums[l+i];
+        }
+        for(int i = 0; i < n2; i++){
+            b[i] = nums[mid+1+i];
+        }
+        int i = 0, k = l;
+        j = 0;
+        while(i < n1 && j < n2){
+            if(a[i] <= b[j]){
+                nums[k] = a[i];
+                k++; i++;
+            }
+            else{
+                nums[k] = b[j];
+                k++; j++;
+            }
+        }
+        while(i < n1){
+            nums[k] = a[i];
+            k++; i++;
+        }
+        while(i < n2){
+            nums[k] = b[j];
+            k++; j++;
+        }
+        
+        return pairs;        
+    }
+    int mergeSort(vector<int>& nums, int l, int r){
+        int pairs = 0;
+        if(l < r){
+            int mid = (l+r)/2;
+            pairs += mergeSort(nums,l,mid);
+            pairs += mergeSort(nums,mid+1,r);
+            
+            pairs += merge(nums,l,mid,r);
+        }
+        return pairs;
+    }
+    int reversePairs(vector<int>& nums) {
+        int n = nums.size();
+        return mergeSort(nums,0,n-1);
+    }
+};
