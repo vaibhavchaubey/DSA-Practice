82576f43c7a4fa02dd877141af5505ebddfcd45e
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-11 15:51:30 +0530

    Time: 138 ms (82.92%), Space: 33.7 MB (98.22%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,63 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s;
-        for(auto num : nums){
-            s.insert(num);
+        if(nums.size() == 0){
+            return 0;
         }
+        sort(nums.begin(), nums.end());
+        int longestStreak = 1; 
+        int prev = nums[0];
+        int currentStreak = 1;
         
-        int longestStreak = 0;
-        for(auto num : nums){
-            if(!s.count(num-1)){     //number lesser than num exists or not
-                int currentNum = num;
-                int currentStreak = 1;
-                while(s.count(currentNum+1)){   //checking if next number exists or not
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-                longestStreak = max(longestStreak, currentStreak);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == prev+1){
+                currentStreak++;
             }
+            else if(nums[i] != prev){
+                currentStreak = 1;
+            }
+            prev = nums[i];
+            longestStreak = max(longestStreak,currentStreak);
         }
         
         return longestStreak;
     }
 };
 
-/*
-Pushing element in set + Iteration do over array + For while loop iteration
-TC - O(N) + O(N) + O(N)    
-SC - O(N)
-*/
-
-
-
-
-//OR (Brute Force)
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         if(nums.size() == 0){
-//             return 0;
-//         }
-//         sort(nums.begin(), nums.end());
-//         int count = 1, cnt = 1;
-//         for(int i = 1; i < nums.size(); i++){
-//             if(nums[i] != nums[i-1]){
-//                 if(nums[i] == nums[i-1]+1){
-//                     cnt++;
-//                 }
-//                 else{
-//                     count = max(count,cnt);
-//                     cnt = 1;
-//                 }
-//             }
-//         }
-        
-//         return max(count,cnt);
-//     }
-// };
-
-//TC - O(N logN) + O(N)
-
-
+//Time Complexity: O(NlogN) + O(N)
+//Space Complexity: O(1)
