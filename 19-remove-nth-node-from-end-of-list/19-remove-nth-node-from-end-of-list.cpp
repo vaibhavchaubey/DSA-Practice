29a60dc934f4ffbe0daa4af71d0a4fedddcd07a4
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-20 00:52:43 +0530

    Time: 8 ms (46.24%), Space: 10.7 MB (75.09%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,35 +10,70 @@
  */
 class Solution {
 public:
-    int length(ListNode* head){
-        int l=0;
-        ListNode* temp = head;
-        while (temp!=NULL){
-            l++;
-            temp = temp->next;
-        }
-        return l;
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(head == NULL){
             return head;
         }
-        int l = length(head);
-        int pos = l-n+1;
-        if(l==1 || pos==1){
-            return head->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        for(int i = 0; i < n; i++){
+            fast = fast->next;
         }
-
-        ListNode* temp = head;
-        int count = 1;
-        while(count!=pos-1){
-            temp = temp->next;
-            count++;
+        if(fast == NULL){
+            ListNode*node=head;
+            head=head->next;
+            delete node;
+            return head;
         }
-        ListNode* todelete = temp->next;
-        temp->next = temp->next->next;
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* todelete = slow->next;
+        slow->next= slow->next->next;
         delete todelete;
         
         return head;
     }
 };
+
+
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     int length(ListNode* head){
+//         int l=0;
+//         ListNode* temp = head;
+//         while (temp!=NULL){
+//             l++;
+//             temp = temp->next;
+//         }
+//         return l;
+//     }
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         if(head == NULL){
+//             return head;
+//         }
+//         int l = length(head);
+//         int pos = l-n+1;
+//         if(l==1 || pos==1){
+//             return head->next;
+//         }
+
+//         ListNode* temp = head;
+//         int count = 1;
+//         while(count!=pos-1){
+//             temp = temp->next;
+//             count++;
+//         }
+//         ListNode* todelete = temp->next;
+//         temp->next = temp->next->next;
+//         delete todelete;
+        
+//         return head;
+//     }
+// };
