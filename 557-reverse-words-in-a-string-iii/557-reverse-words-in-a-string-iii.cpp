2d17c0b993e637319d603675ff73a30782fa1dbd
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-09-22 18:24:11 +0530

    Time: 44 ms (18.98%), Space: 9.7 MB (45.42%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,23 +1,51 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans = "";
         int n = s.size();
-        string str = "";
-        for(int i = 0; i < n ; i++){
-            if(s[i] == ' '){
-                reverse(str.begin(), str.end());
-                ans += str;
-                ans += " ";
-                str = "";
-            }
-            else{
-                str += s[i];
+        int lastSpaceIndex = -1;
+        for(int i = 0; i <= n ; i++){
+            if(i == n || s[i] == ' '){
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = i - 1;
+                while(startIndex < endIndex){
+                    swap(s[startIndex], s[endIndex]);
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = i;
             }
         }
-        reverse(str.begin(), str.end());
-        ans += str;
         
-        return ans;
+        return s;
     }
 };
+
+
+
+
+
+//OR
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string ans = "";
+//         int n = s.size();
+//         string str = "";
+//         for(int i = 0; i < n ; i++){
+//             if(s[i] == ' '){
+//                 reverse(str.begin(), str.end());
+//                 ans += str;
+//                 ans += " ";
+//                 str = "";
+//             }
+//             else{
+//                 str += s[i];
+//             }
+//         }
+//         reverse(str.begin(), str.end());
+//         ans += str;
+        
+//         return ans;
+//     }
+// };
