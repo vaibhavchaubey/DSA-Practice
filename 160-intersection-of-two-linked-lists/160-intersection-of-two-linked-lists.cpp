0ebd1359515aa76e36f3b4dae25ef56f5da98db8
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-10-07 22:52:27 +0530

    Time: 106 ms (33.13%), Space: 14.4 MB (88.53%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,46 +8,97 @@
  */
 class Solution {
 public:
-    int length(ListNode* head){
-        ListNode* temp = head;
-        int l = 0;
-        while(temp != NULL){
-            l++;
-            temp = temp->next;
-        }
-        return l;
-    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int l1 = length(headA);
-        int l2 = length(headB);
+        ListNode* d1 = headA;
+        ListNode* d2 = headB;
         
-        int d = 0;
-        ListNode* ptr1;
-        ListNode* ptr2;
-        if(l1 > l2){
-            d = l1-l2;
-            ptr1 = headA;
-            ptr2 = headB;
-        }
-        else{
-            d = l2-l1;
-            ptr1 = headB;
-            ptr2 = headA;
-        }
-        while(d){
-            ptr1 = ptr1->next;
-            if(ptr1 == NULL){
-                return NULL;
+        while(d1 != d2){
+            if(d1 == NULL){
+                d1 = headB;
             }
-            d--;
-        }
-        while(ptr1 != NULL && ptr2 != NULL){
-            if(ptr1 == ptr2){
-                return ptr1;
+            else{
+                d1 = d1->next;
             }
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
+            
+            if(d2 == NULL){
+                d2 = headA;
+            }
+            else{
+                d2 = d2->next;
+            }
+            
         }
-        return NULL;
+        return d1;
     }
 };
+
+
+//OR
+
+// class Solution {
+// public:
+//     int length(ListNode* head){
+//         ListNode* temp = head;
+//         int l = 0;
+//         while(temp != NULL){
+//             l++;
+//             temp = temp->next;
+//         }
+//         return l;
+//     }
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         int l1 = length(headA);
+//         int l2 = length(headB);
+        
+//         int d = 0;
+//         ListNode* ptr1;
+//         ListNode* ptr2;
+//         if(l1 > l2){
+//             d = l1-l2;
+//             ptr1 = headA;
+//             ptr2 = headB;
+//         }
+//         else{
+//             d = l2-l1;
+//             ptr1 = headB;
+//             ptr2 = headA;
+//         }
+//         while(d){
+//             ptr1 = ptr1->next;
+//             if(ptr1 == NULL){
+//                 return NULL;
+//             }
+//             d--;
+//         }
+//         while(ptr1 != NULL && ptr2 != NULL){
+//             if(ptr1 == ptr2){
+//                 return ptr1;
+//             }
+//             ptr1 = ptr1->next;
+//             ptr2 = ptr2->next;
+//         }
+//         return NULL;
+//     }
+// };
+
+
+
+// OR
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         unordered_set<ListNode*> s;
+//         while(headA != NULL){
+//             s.insert(headA);
+//             headA = headA->next;
+//         }
+//         while(headB != NULL){
+//             if(s.find(headB) != s.end()){
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+//         return NULL;
+//     }
+// };
