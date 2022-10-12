04fb9326546a710c1c1e64b77ba17fa75b954bbf
Vaibhav Chaubey <88048439+vaibhavchaubey@users.noreply.github.com>
2022-10-12 23:16:28 +0530

    Time: 36 ms (38.77%), Space: 19.7 MB (81.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,27 +2,64 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int left[n];
-        int right[n]; 
-        
-        left[0] = height[0];
-        for(int i = 1; i < n; i++){
-            left[i] = max(left[i-1], height[i]);
-        }
-        
-        right[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-            right[i] = max(right[i+1], height[i]);
-        }
-        
+        int left = 0, right = n-1;
         int waterTrapped = 0;
-        for(int i = 0; i < n; i++){
-            waterTrapped += (min(left[i],right[i]) - height[i]);
+        int leftMax = 0, rightMax = 0;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            }
+            else{                               //height[right] > height[left]
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
         }
         
         return waterTrapped;
     }
 };
-
 //Time Complexity: O(N)                                           
-//Space Complexity: O(N)
+//Space Complexity: O(1)
+
+// OR
+
+//Time Complexity: O(3*N)                                           
+//Space Complexity: O(N)+O(N)
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         int left[n];
+//         int right[n]; 
+        
+//         left[0] = height[0];
+//         for(int i = 1; i < n; i++){
+//             left[i] = max(left[i-1], height[i]);
+//         }
+        
+//         right[n-1] = height[n-1];
+//         for(int i = n-2; i >= 0; i--){
+//             right[i] = max(right[i+1], height[i]);
+//         }
+        
+//         int waterTrapped = 0;
+//         for(int i = 0; i < n; i++){
+//             waterTrapped += (min(left[i],right[i]) - height[i]);
+//         }
+        
+//         return waterTrapped;
+//     }
+// };
+
